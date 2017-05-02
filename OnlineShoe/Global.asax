9b493708181d32af83c7678cb62267eb9a27a5d0
kemshiro <i.am.nguyentuanlinh@gmail.com>
2017-05-02 17:44:04 +0700

    Add project files.

---
+++ OnlineShoe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineShoe.MvcApplication" Language="C#" %>
---
+++ OnlineShoe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineShoe.Startup))]
+namespace OnlineShoe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
